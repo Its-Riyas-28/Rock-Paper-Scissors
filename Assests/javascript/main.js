0ae451376548b1568e097d9eb113870c6a809a52
Its-Riyas-28 <Its.mohd.riyas@gmail.com>
2024-09-03 23:36:23 +0530

    Added Rules Module

--- Assests/javascript/main.js
+++ Assests/javascript/main.js
@@ -1,5 +1,23 @@
-// Existing JavaScript code...
+// RULES MODULE
+document.getElementById("rules-button").addEventListener("click", function () {
+  const rules = document.querySelector(".rules");
+  const closeButton = document.querySelector(".close-button");
 
+  if (rules.style.transform === "scale(1)") {
+    rules.style.transform = "scale(0)";
+    closeButton.style.display = "none";
+  } else {
+    rules.style.transform = "scale(1)";
+    closeButton.style.display = "flex";
+  }
+});
+
+document.getElementById("close-button").addEventListener("click", function () {
+  document.querySelector(".rules").style.transform = "scale(0)";
+  document.querySelector(".close-button").style.display = "none";
+});
+
+// Gameplay
 const CHOICES = [
   {
     name: "paper",
